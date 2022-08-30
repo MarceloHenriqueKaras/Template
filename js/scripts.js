8ce22549230b2a8952d737f71df3e0cd05b5255b
Marcelo Henrique Karas <101262700+MarceloHenriqueKaras@users.noreply.github.com>
2022-08-30 00:27:23 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+evento = document.getElementById("peido")
+// Ambiência da vaca maldita a medonha a amedrontadora aquela que traz dor e destruição e morte e sanguinolencia e malditagem
+const audio = new Audio("/assets/boom.mp3")
+audio.play()
+
+// peido
+evento.onclick = function peida() {
+    const audio1 = new Audio("/assets/fart.mp3")
+    audio1.play()
+}
+
+
